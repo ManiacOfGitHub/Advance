cf511868af257ff6b6e87a2d45fb5087aa9de4d4
ManiacOfGitHub <06crhenry@gmail.com>
2020-11-13 16:22:37 +0000

    Added Socket.IO

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
+var server = require('http').createServer(app)
+var io = require("socket.io")(server);
+
+app.use("/", express.static(__dirname + "/web/js"))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/web/index.html");
 });
 
-var listener = app.listen(3000, () => {
+io.on("connection", (socket) => {
+    socket.on("test", (data) => {
+        console.log(data);
+    })
+});
+var listener = server.listen(3000, () => {
     console.log("The server is listening on port " + listener.address().port);
 })
