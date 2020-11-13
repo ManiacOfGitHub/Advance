ee5d9c241ea3c8dea82361243fdd850382ebaa6c
ManiacOfGitHub <06crhenry@gmail.com>
2020-11-13 15:13:56 +0000

    Add Test Express Server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("This is a test to see how Gitpod works.")
+var express = require("express");
+var app = express();
+app.get("/", (req, res) => {
+    res.send("Hi");
+});
+
+var listener = app.listen(3000, () => {
+    console.log("The server is listening on port " + listener.address().port);
+})
